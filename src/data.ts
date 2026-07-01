9832aa5713fb4f3ebeac2a7f16ab6800cf23c780
laulau030416-create <laulau.030416@gmail.com>
2026-06-30 21:16:36 -0400

    feat: update portfolio images to AVIF format
    Add sharp dependency and convert website screenshots to AVIF for better performance and compression.

--- src/data.ts
+++ src/data.ts
@@ -1,8 +1,8 @@
 import { WebScreenshot } from './types';
-import imgSpace from './assets/images/web_space_logistics_1782861206611.jpg';
-import imgNexori from './assets/images/web_nexori_finance_1782861215529.jpg';
-import imgLuma from './assets/images/web_luma_emotional_1782861224125.jpg';
-import imgAcru from './assets/images/web_acru_wealth_1782861236968.jpg';
+import imgSpace from './assets/images/web_space_logistics_1782861206611.avif';
+import imgNexori from './assets/images/web_nexori_finance_1782861215529.avif';
+import imgLuma from './assets/images/web_luma_emotional_1782861224125.avif';
+import imgAcru from './assets/images/web_acru_wealth_1782861236968.avif';
 
 export const webScreenshots: WebScreenshot[] = [
   {
